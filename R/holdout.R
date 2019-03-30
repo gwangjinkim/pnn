a5c62e09755ebde11b75fc784dd1cd55b187ced3
Gwang-Jin Kim <32566039+gwangjinkim@users.noreply.github.com>
2019-03-31 00:48:57 +0100

    small non-essential improvements in code

--- R/holdout.R
+++ R/holdout.R
@@ -1,13 +1,16 @@
 # Hold-out method
+# Like a Python generator returns data frame row by row in `one` but all other rows in `rest`.
+# For cross-validation useful.
 # @param df A data-frame
 hold_out <- function(df) {
     i <- 0
-    imax <- length(df[,1])
+    imax <- nrow(df)
     function() {
         i <<- i + 1
-        if(i > imax) return(NA)
-        one <- df[i,]
-        rest <- df[-i,]
-        return(list(one=one,rest=rest))
+        if(i > imax) {
+            return(NA)
+        } else {
+            return(list(one=df[i,],rest=df[-i,]))
+        }
     }
 }
